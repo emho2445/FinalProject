45675281cd945d1b832865c50ba6a12517d281e7
Emma Hopson <emmaehopson@icloud.com>
2023-10-25 20:25:22 -0600

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Emma  Hopson on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
