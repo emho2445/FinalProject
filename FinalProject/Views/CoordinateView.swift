e4f0b87b92451a528b46c9686ceb783963c6b17b
Emma Hopson <emmaehopson@icloud.com>
2023-12-17 22:45:51 -0700

    Fixing coordinate button and scrollview

--- FinalProject/Views/CoordinateView.swift
+++ FinalProject/Views/CoordinateView.swift
@@ -11,9 +11,11 @@ struct CoordinateView: View {
     @State var lat: String = ""
     @State var lng: String = ""
 
+    @State var validCoordinates = true
     
 
     var body: some View {
+        
             ZStack(content: {
                 Image("Sunset")
                     .resizable()
@@ -27,80 +29,84 @@ struct CoordinateView: View {
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
                     .opacity(0.00)
-                
-                VStack{
-                    Spacer()
-                    
-                    Group{
-                        Text("Enter a LATITUDE coordinate. This is a number between -90 and 90.")
-                        TextField("Latitude", text: $lat)
+                ScrollView{
+                    VStack{
+                        Spacer()
+                            .frame(height: 120)
                         
-                        Text("Enter a LONGITUDE coordinate. This is a number between -180 and 180.")
-                        TextField("Longitude", text: $lng)
-                    }
-                    .frame(minWidth: 300, idealWidth: 300, maxWidth: 300)
-                    
-                    Spacer()
-                        .frame(height: 200)
-                    
-                    
-                    if lat != "", lng != "" {
+                        Group{
+                            Text("Enter a LATITUDE coordinate. This is a number between -90 and 90.")
+                            TextField("Latitude", text: $lat)
+                            
+                            Text("Enter a LONGITUDE coordinate. This is a number between -180 and 180.")
+                            TextField("Longitude", text: $lng)
+                        }
+                        .frame(minWidth: 300, idealWidth: 300, maxWidth: 300)
                         
-                        if let doubleLat = Double(lat), let doubleLng = Double(lng){
-                            if doubleLat < -90{
+                        Spacer()
+                            .frame(height: 180)
+                        
+                        
+                        if lat != "", lng != "" {
+                            
+                            if let doubleLat = Double(lat), let doubleLng = Double(lng){
+                                if doubleLat < -90{
+                                    Text("ERROR!")
+                                        .foregroundColor(.red)
+                                    Text("Value entered for Latitude is invalid")
+                                        .foregroundColor(.red)
+                                } else if doubleLat > 90{
+                                    Text("ERROR!")
+                                        .foregroundColor(.red)
+                                    Text("Value entered for Latitude is invalid")
+                                        .foregroundColor(.red)
+                                } else if doubleLng < -180{
+                                    Text("ERROR!")
+                                        .foregroundColor(.red)
+                                    Text("Value entered for Longitude is invalid")
+                                        .foregroundColor(.red)
+                                } else if doubleLng > 180{
+                                    Text("ERROR!")
+                                        .foregroundColor(.red)
+                                    Text("Value entered for Longitude is invalid")
+                                        .foregroundColor(.red)
+                                } else {
+                                    NavigationLink(destination: MapView(lat: doubleLat, lng: doubleLng)){
+                                        Text("See Details ->")
+                                            .customNavigationLink()
+                                    }.disabled(!validCoordinates)
+                                }
+                                
+                                
+                            }else{
                                 Text("ERROR!")
                                     .foregroundColor(.red)
-                                Text("Value entered for Latitude is invalid")
-                                    .foregroundColor(.red)
-                            }else if doubleLat > 90{
-                                Text("ERROR!")
-                                    .foregroundColor(.red)
-                                Text("Value entered for Latitude is invalid")
+                                Text("Values entered are invalid")
                                     .foregroundColor(.red)
                             }
                             
-                            if doubleLng < -180{
-                                Text("ERROR!")
-                                    .foregroundColor(.red)
-                                Text("Value entered for Longitude is invalid")
-                                    .foregroundColor(.red)
-                            }else if doubleLng > 180{
-                                Text("ERROR!")
-                                    .foregroundColor(.red)
-                                Text("Value entered for Longitude is invalid")
-                                    .foregroundColor(.red)
-                            }
                             
-                            NavigationLink(destination: MapView(lat: doubleLat, lng: doubleLng)){
-                                Text("See Details ->")
-                                    .customNavigationLink()
-                            }
-                        }else{
-                            Text("ERROR!")
-                                .foregroundColor(.red)
-                            Text("Values entered are invalid")
-                                .foregroundColor(.red)
+                        }
+                        Spacer()
+                            .frame(height: 25.0)
+                        
+                        Button{
+                            lat = ""
+                            lng = ""
+                        }label: {
+                            Text("Clear Fields")
+                                .customNavigationLink()
                         }
                         
-                        
+                        //Spacer()
                     }
-                    Spacer()
-                        .frame(height: 25.0)
-                    
-                    Button{
-                        lat = ""
-                        lng = ""
-                    }label: {
-                        Text("Clear Fields")
-                            .customNavigationLink()
-                    }
-                    
-                    Spacer()
-                    
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    //.background(.ultraThinMaterial)
                 }
-                .padding()
-                .frame(maxWidth: .infinity)
-                //.background(.ultraThinMaterial)
+
+                    
+
             })
 
         .navigationTitle("Set Location")
